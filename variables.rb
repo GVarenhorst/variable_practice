9253757c7eff963970a73701f50476ad66547b5f
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-02-11 18:53:53 -0600

    Correct the assignment with put statements

--- variables.rb
+++ variables.rb
@@ -1,27 +1,42 @@
 # Data Types
 
-## Strings
+#Strings
 
 name = "harold"
 city = "new_york"
 pet_animal = "cat"
 car = "toyota"
 
-## Integers
+ p name
+ p city
+ p pet_animal
+ p car
+# Integers
 
 birth_year = 1975
 age = 47
 zipcode = 64030
 
-##  Floats
+p birth_year
+p age
+p zipcode
+
+#  Floats
 
 account_balance = 429.17
 degree_temp = 98.5
 loan_value = 16800.00
 
-## Booleans
+p account_balance
+p degree_temp
+p loan_value
+
+# Booleans
 
 down_payment = false
 married = true
 deceased = false
 
+p down_payment
+p married
+p deceased
